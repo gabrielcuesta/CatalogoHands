78139fd534b59448bd88b6e588d9322e13d83b72
Hugo Fusinato <hugohvf@hotmail.com>
2020-11-18 22:19:41 -0300

    fix gameSheet

--- lib/games.ts
+++ lib/games.ts
@@ -1,6 +1,8 @@
-import { Game } from "../types/game";
+import { Game } from '../types/game';
+
+const gameSheet = process.env.GAME_SHEET_URL ?? '';
 
 export const getGames = async (): Promise<Game[]> => {
-  const res = await fetch(process.env.GAME_SHEET_URL);
+  const res = await fetch(gameSheet);
   return await res.json();
 };
